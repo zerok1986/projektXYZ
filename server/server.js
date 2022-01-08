a3fc54f3a60638971605f2b5a6fa2ecff017cec3
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-09 00:00:49 +0100

    build(server): modify server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-const app = require("./app");
-
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 5005;
+const app = require('./app')
+const PORT = process.env.PORT || 5005
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+  console.info(`🏃 on port http://localhost:${PORT}`)
+})
